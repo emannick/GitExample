999359d74932c8d2b475771a64e91a7b596bf922
Amit Shetye <AmitS@wsmum80208.local>
2016-07-13 11:28:36 +0530

    Initial Commit

---
+++ GitExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitExample
+//
+//  Created by Amit Shetye on 7/13/16.
+//  Copyright © 2016 Amit Shetye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
